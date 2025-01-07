c113420cfdd62bd739b86b2fb415c50e151e36e9
plzbugmenot <plzbugmenot>
2025-01-07 13:53:54 -0600

    [feat] data types

---
+++ 3. Common Programming Concepts/3.2 Data Types.rs
@@ -0,0 +1,35 @@
+fn main() {
+  let x = 2.0; // f64
+
+  let y: f32 = 3.0; // f32
+
+      // addition
+      let sum = 5 + 10;
+
+      // subtraction
+      let difference = 95.5 - 4.3;
+  
+      // multiplication
+      let product = 4 * 30;
+  
+      // division
+      let quotient = 56.7 / 32.2;
+      let truncated = -5 / 3; // Results in -1
+  
+      // remainder
+      let remainder = 43 % 5;
+
+      let t = true;
+
+      let f: bool = false; // with explicit type annotation
+
+      let c = 'z';
+      let z: char = 'ℤ'; // with explicit type annotation
+      let heart_eyed_cat = '😻';
+
+
+      let months = ["January", "February", "March", "April", "May", "June", "July",
+              "August", "September", "October", "November", "December"];
+      let a: [i32; 5] = [1, 2, 3, 4, 5];
+      let a = [3; 5]; // let a = [3, 3, 3, 3, 3];
+}
